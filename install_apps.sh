4859b4dad79e85a47ba6af40f9263d713ecf4906
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-25 21:44:18 +0400

    Update install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-app_list=" xf86-video-intel xf86-video-amdgpu xorg sddm bluedevil discover smplayer smplayer-themes bleachbit nasc google-chrome ncdu discord conky conky-manager 
-flameshot python-pip otpclient ksysguard notion-app-enhanced masterpdfeditor-free freeoffice stacer-bin teamviewer backintime
-xdotool libinput-gestures plasma-browser-integration plasma-desktop plasma-nm plasma-pa nemo konsole kdeplasma-addons nemo-fileroller 
-nemo-preview nemo-share xdg-user-dirs khotkeys packagekit-qt5 wget powerdevil sddm-kcm systemsettings xdg-desktop-portal-kde gthumb gedit kdeconnect sshfs okular
-noto-fonts noto-fonts-cjk ttf-dejavu ttf-liberation ttf-opensans"
+app_list=" xf86-video-intel xf86-video-amdgpu xorg sddm bluedevil smplayer smplayer-themes bleachbit nasc google-chrome ncdu discord flameshot 
+python-pip otpclient ksysguard notion-app-enhanced masterpdfeditor-free freeoffice stacer-bin teamviewer backintime
+xdotool libinput-gestures nemo nemo-fileroller nemo-preview nemo-share packagekit-qt5 wget gthumb gedit
+kde-applications xdg-user-dirs packagekit-qt5"
 yay -Syyu $app_list --noconfirm
 systemctl enable ssdm --noconfirm 
 echo "---------------------------------------"
