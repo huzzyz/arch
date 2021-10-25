61beea66aa47c6c7ee7bc36a47707e7e9888ef40
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-25 18:48:24 +0400

    Update yaya.sh

--- yaya.sh
+++ yaya.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo packman -Syyu git --noconfirm
 cd /opt && git clone https://aur.archlinux.org/yay-git.git
 sudo chown -R $USER:$GROUP ./yay-git
 cd yay-git && makepkg -si --noconfirm
