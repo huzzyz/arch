d2136a01773af23cc3cb8c72cc1c92a340bc50c2
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-25 18:54:33 +0400

    Update install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-app_list="bluedevil discover smplayer smplayer-themes bleachbit nasc google-chrome ncdu discord conky conky-manager 
+app_list=" xf86-video-intel xf86-video-amdgpu xorg sddm bluedevil discover smplayer smplayer-themes bleachbit nasc google-chrome ncdu discord conky conky-manager 
 flameshot python-pip otpclient ksysguard notion-app-enhanced masterpdfeditor-free freeoffice stacer-bin teamviewer backintime
 xdotool libinput-gestures plasma-browser-integration plasma-desktop plasma-nm plasma-pa nemo konsole kdeplasma-addons nemo-fileroller 
 nemo-preview nemo-share xdg-user-dirs packagekit-qt5"
-
-yay -Syyu grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober mtools dosfstools base-devel linux-headers xf86-video-intel xf86-video-amdgpu xorg sddm --noconfirm
+yay -Syyu $app_list --noconfirm
 systemctl enable ssdm --noconfirm 
-yay -S $app_list --noconfirm
 echo "---------------------------------------"
 echo "---------------------------------------"
 echo "---------------------------------------"
