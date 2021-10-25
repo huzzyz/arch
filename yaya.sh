4585651ae81851031f01adf0aff16552980fb6cd
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-25 17:58:45 +0400

    Update yaya.sh
    installation of yay

--- yaya.sh
+++ yaya.sh
@@ -3,13 +3,12 @@ sudo packman -Syyu git --noconfirm
 cd /opt && git clone https://aur.archlinux.org/yay-git.git
 sudo chown -R $USER:$GROUP ./yay-git
 cd yay-git && makepkg -si --noconfirm
-yay --sudoloop --save
+yay --sudoloop --save && cd ~/
 echo "-------------------------------------------"
 echo "-------------------------------------------"
 echo "-------------------------------------------"
 echo "-------------------------------------------"
 echo "----------------FINISHED-------------------"
-echo "-------------------------------------------"
 echo "-------------------------------------------"
 echo "-------------------------------------------"
 echo "-------------------------------------------"
