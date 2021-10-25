6e8ea56c9879622b1ddb77b6b3ff66ae47ebbdbd
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-25 20:34:00 +0400

    Update install_apps.sh
    fonts, added

--- install_apps.sh
+++ install_apps.sh
@@ -2,7 +2,8 @@
 app_list=" xf86-video-intel xf86-video-amdgpu xorg sddm bluedevil discover smplayer smplayer-themes bleachbit nasc google-chrome ncdu discord conky conky-manager 
 flameshot python-pip otpclient ksysguard notion-app-enhanced masterpdfeditor-free freeoffice stacer-bin teamviewer backintime
 xdotool libinput-gestures plasma-browser-integration plasma-desktop plasma-nm plasma-pa nemo konsole kdeplasma-addons nemo-fileroller 
-nemo-preview nemo-share xdg-user-dirs packagekit-qt5 wget powerdevil sddm-kcm systemsettings xdg-desktop-portal-kde gthumb"
+nemo-preview nemo-share xdg-user-dirs packagekit-qt5 wget powerdevil sddm-kcm systemsettings xdg-desktop-portal-kde gthumb gedit kdeconnect sshfs okular
+noto-fonts noto-fonts-cjk ttf-dejavu ttf-liberation ttf-opensans"
 yay -Syyu $app_list --noconfirm
 systemctl enable ssdm --noconfirm 
 echo "---------------------------------------"
